ca3131155dfcadd56b9e9e699ae65e1eaa24946f
lucdenardi <denardiluc@gmail.com>
2026-04-02 09:53:43 -0600

    added multilingual data dict

--- data/get_data.py
+++ data/get_data.py
@@ -1,5 +1,7 @@
 from datasets import load_dataset
 import kagglehub
+import torch
+from torch.utils.data import TensorDataset, DataLoader
 
 def get_data():
     japanese_train = load_dataset("asahi417/jvnv-emotional-speech-corpus",split="train")
@@ -8,5 +10,14 @@ def get_data():
     english_train = load_dataset("En1gma02/english_emotions", split="train")
     spanish = kagglehub.dataset_download("angeluxarmenta/ses-sd")
     arabic = kagglehub.dataset_download("a13x10/basic-arabic-vocal-emotions-dataset")
+
+    multiling_data = {
+        "jap": japanese_train,
+        "ban": bangla_train,
+        "ch": chinese_train,
+        "eng": english_train,
+        "span": spanish,
+        "ar": arabic,
+    }
 
     return japanese_train, bangla_train, chinese_train, english_train, spanish, arabic

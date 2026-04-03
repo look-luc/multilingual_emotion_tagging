ab2a0645fe03654e09c4bd70e8297f3f26a58ebb
lucdenardi <denardiluc@gmail.com>
2026-04-03 16:31:11 -0600

    trying to get all the data

--- data/get_data.py
+++ data/get_data.py
@@ -5,22 +5,20 @@ from torch.utils.data import TensorDataset, DataLoader
 
 def get_data():
     japanese_train = load_dataset("asahi417/jvnv-emotional-speech-corpus",split="test")
-    print(japanese_train)
+    print(f"japanese: {japanese_train}")
     bangla_train = load_dataset(
         "json",
         data_files="https://huggingface.co/datasets/sustcsenlp/bn_emotion_speech_corpus/resolve/main/train.jsonl",
         split="train"
     )
-    print(bangla_train)
+    print(f"bangla: {bangla_train}")
     chinese_train = load_dataset("BillyLin/CASIA_speech_emotion_recognition", split="train")
-    print(chinese_train)
+    print(f"chinese: {chinese_train}")
     english_train = load_dataset("En1gma02/english_emotions", split="train")
-    print(english_train)
+    print(f"english: {english_train}")
     spanish = kagglehub.dataset_download("angeluxarmenta/ses-sd")
-    print(spanish)
+    print(f"spanish: {spanish}")
     arabic = kagglehub.dataset_download("a13x10/basic-arabic-vocal-emotions-dataset")
-    print(arabic)
-
-
+    print(f"arabic: {arabic}")
 
     return japanese_train, bangla_train, chinese_train, english_train, spanish, arabic

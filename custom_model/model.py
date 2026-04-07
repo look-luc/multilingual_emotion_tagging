ac98bb226ab6eeb96bacf231f8bf6ea61c6b5497
lucdenardi <denardiluc@gmail.com>
2026-04-07 13:36:12 -0600

    made model and made training loop

--- custom_model/model.py
+++ custom_model/model.py
@@ -3,12 +3,11 @@ from transformers import Wav2Vec2Model, Wav2Vec2Processor
 import torch.nn as nn
 
 class MultiLingEmotion(nn.Module):
-    def __init__(self, input):
+    def __init__(self):
         super(MultiLingEmotion, self).__init__()
 
-        self.input = input
         self.vocab_size = 7
-        self.vocab = ["angry", "happy", "sad", "neutral", "fear", "disgust", "surprise"]
+        self.target_emotions = ["angry", "happy", "sad", "neutral", "fear", "disgust", "surprise"]
 
         self.encoder = Wav2Vec2Model.from_pretrained(
             "facebook/wav2vec2-large-960h-lv60-self",
@@ -16,9 +15,11 @@ class MultiLingEmotion(nn.Module):
             attn_implementation="flash_attention_2"
         )
 
-        self.pooling = nn.AvgPool2d(kernel_size=2, stride=2)
-
         self.linear = nn.Linear(self.encoder.config.hidden_size, self.vocab_size)
 
-    def forward(self, x, target):
-        pass
+    def forward(self, x):
+        x = self.encoder(x)
+        x = x.last_hidden_state
+        x = torch.mean(x, dim=1)
+        x = self.linear(x)
+        return x

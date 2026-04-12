f3913556a3cc70dfb7558b48c4ad0b51d110119e
lucdenardi <denardiluc@gmail.com>
2026-04-12 13:24:48 -0600

    added a way to keep track of fine-tuned models

--- custom_model/model.py
+++ custom_model/model.py
@@ -12,7 +12,7 @@ class MultiLingEmotion(nn.Module):
         self.num_classes = len(self.target_emotions)
 
         self.audio_encoder = Wav2Vec2Model.from_pretrained(
-            "jonatasgrosman/wav2vec-xls-r-300m-multilingual",
+            "facebook/wav2vec2-large-960h-lv60-self",
             attn_implementation="eager"
         )
 
